75efc7721adcad273d27ca088318ad28cb8d1ad7
Arvydas Sidorenko <asido@chessclub.com>
2016-06-09 17:23:17 +0200

    Coding style fixes.

--- EpubSharp/Readers/RootFilePathReader.cs
+++ EpubSharp/Readers/RootFilePathReader.cs
@@ -13,10 +13,15 @@ namespace EpubSharp.Readers
             const string epubContainerFilePath = "META-INF/container.xml";
             var containerFileEntry = epubArchive.GetEntryIgnoringSlashDirection(epubContainerFilePath);
             if (containerFileEntry == null)
+            {
                 throw new Exception($"EPUB parsing error: {epubContainerFilePath} file not found in archive.");
+            }
+
             XmlDocument containerDocument;
             using (var containerStream = containerFileEntry.Open())
+            {
                 containerDocument = XmlUtils.LoadDocument(containerStream);
+            }
             var xmlNamespaceManager = new XmlNamespaceManager(containerDocument.NameTable);
             xmlNamespaceManager.AddNamespace("cns", "urn:oasis:names:tc:opendocument:xmlns:container");
             var rootFileNode = containerDocument.DocumentElement.SelectSingleNode("/cns:container/cns:rootfiles/cns:rootfile", xmlNamespaceManager);
