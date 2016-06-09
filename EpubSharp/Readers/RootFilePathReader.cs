61db018a9d98026656da3d46dd1b9b430defe988
Arvydas Sidorenko <asido@chessclub.com>
2016-06-09 14:41:42 +0200

    Coding style fixes.

--- EpubSharp/Readers/RootFilePathReader.cs
+++ EpubSharp/Readers/RootFilePathReader.cs
@@ -10,10 +10,10 @@ namespace EpubSharp.Readers
     {
         public static string GetRootFilePath(ZipArchive epubArchive)
         {
-            const string EPUB_CONTAINER_FILE_PATH = "META-INF/container.xml";
-            ZipArchiveEntry containerFileEntry = epubArchive.GetEntry(EPUB_CONTAINER_FILE_PATH);
+            const string epubContainerFilePath = "META-INF/container.xml";
+            ZipArchiveEntry containerFileEntry = epubArchive.GetEntry(epubContainerFilePath);
             if (containerFileEntry == null)
-                throw new Exception(String.Format("EPUB parsing error: {0} file not found in archive.", EPUB_CONTAINER_FILE_PATH));
+                throw new Exception(string.Format("EPUB parsing error: {0} file not found in archive.", epubContainerFilePath));
             XmlDocument containerDocument;
             using (Stream containerStream = containerFileEntry.Open())
                 containerDocument = XmlUtils.LoadDocument(containerStream);
