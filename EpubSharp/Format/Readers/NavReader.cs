a96096e56887be8740c3725de6108b7535d3120a
Arvydas Sidorenko <asido@chessclub.com>
2016-08-31 11:09:44 +0200

    Created NavDocument and NavReader files.

---
+++ EpubSharp/Format/Readers/NavReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace EpubSharp.Format.Readers
+{
+    internal static class NavReader
+    {
+        public static NavDocument Read(XDocument xml)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
