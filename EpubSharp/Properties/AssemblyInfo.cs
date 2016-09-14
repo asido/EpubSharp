ffd661286b94481d7bc8f07a44cad9721da8bc91
Arvydas Sidorenko <asido@chessclub.com>
2016-09-14 15:50:42 +0200

    Added AssemblyDescription.

--- EpubSharp/Properties/AssemblyInfo.cs
+++ EpubSharp/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EpubSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A library for reading and writing EPUB files.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EpubSharp")]
